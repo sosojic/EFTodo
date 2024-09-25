fedc765f2f1493d64f67f2f7d1e71032710e709b
sosojic <148350844+sosojic@users.noreply.github.com>
2024-09-25 14:50:24 +0200

    new

---
+++ EFTodo/Models/Todo.cs
@@ -0,0 +1,7 @@
+public class Todo{
+    public int Id { get; set;}
+    public string? Task { get; set;}
+    public bool Completed { get; set;}
+
+    
+}
--- EFTodo/Program.cs
+++ EFTodo/Program.cs
@@ -1,2 +1,21 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿var todo1= new Todo{
+    Id=4,
+    Task="Wash the dishes",
+    Completed=true
+};
+
+var todo2= new Todo{
+    Id=5,
+    Task="Clean the house",
+    Completed=false
+};
+
+var todo3= new Todo{
+    Id=6,
+    Task="Mow the lawn",
+    Completed=false
+};
+using var context=new TodoContext();
+context.Todo.Add(todo1);
+context.Todo.Add(todo2);
+context.Todo.Add(todo3);
